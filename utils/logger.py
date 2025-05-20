15431bd2cac3029a30e10529cb830d9c1c892106
Pandit <22it3037@rgipt.ac.in>
2025-05-20 11:51:32 +0530

    logger file created

---
+++ utils/logger.py
@@ -0,0 +1,49 @@
+"""
+Logging utility for the application.
+"""
+import logging
+import os
+import sys
+from datetime import datetime
+
+def setup_logger(name, level=logging.INFO):
+    """
+    Set up a logger with the given name and level.
+    
+    Args:
+        name: Logger name
+        level: Logging level
+        
+    Returns:
+        Logger instance
+    """
+    # Create logs directory if it doesn't exist
+    os.makedirs('logs', exist_ok=True)
+    
+    # Create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    
+    # Remove existing handlers
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+    
+    # Create console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(level)
+    
+    # Create file handler
+    log_file = f"logs/{name}_{datetime.now().strftime('%Y%m%d')}.log"
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(level)
+    
+    # Create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    
+    # Add handlers to logger
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    
+    return logger
